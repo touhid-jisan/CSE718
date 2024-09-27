b36f1ceab0c60b799e2979c78f3eb8ab1b3b12d1
touhid-jisan <touhidulislamjisan@gmail.com>
2024-09-27 22:28:07 +0600

    background creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import math
 import random
+from turtle import back, title, width
 import pygame
 
 from os import listdir
@@ -10,7 +11,6 @@ pygame.init()
 
 pygame.display.set_caption("2D Game Development Using Python")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5 #Speed of player
@@ -18,18 +18,42 @@ PLAYER_VEL = 5 #Speed of player
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# get background information
+def get_background(name):
+    image = pygame.image.load(join("assets", "background", name))
+    _, _, width, height = image.get_rect()
+    
+    tiles = []
+    
+    for i in range(WIDTH // width +1):
+        for j in range(HEIGHT // height +1):
+            position = [i * width, j *height]
+            tiles.append(position)
+            
+    return tiles, image
+
+# draw background
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+        
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
     
     run = True
     while run:
-        clock.tick(FPS)  # whill loop run in 60 fps per second
+        clock.tick(FPS)  # run in 60 fps per second
         
         for event in pygame.event.get():
             
             if event.type == pygame.QUIT:
                 run = False
                 break
+            
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
     
