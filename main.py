830344592db200d107a244161e22d952a01d6bd0
touhid-jisan <touhidulislamjisan@gmail.com>
2024-09-28 21:22:57 +0600

    move palyer left right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import math
 import random
-from turtle import back, title, width
 import pygame
 
 from os import listdir
@@ -13,26 +12,53 @@ pygame.display.set_caption("2D Game Development Using Python")
 
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
-PLAYER_VEL = 5 #Speed of player
-
+PLAYER_VEL = 5 # Speed of player
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir_one, dir_two, width, height, direction=False):
+    path = join("assets", dir_one, dir_two)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    
+    all_sprites = {}
+    for img in images:
+        sprite_sheet = pygame.image.load(join(path, img)).convert_alpha()
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+            
+        if direction:
+            all_sprites[img.replace(".png", "")+ "_right"] = sprites
+            all_sprites[img.replace(".png", "")+ "_left"] = flip(sprites)
+        else:
+            all_sprites[img.replace(".png", "")] = sprites
+            
+    return all_sprites    
+            
 # sprite will tell us if objects are colliding each other
 # will handle collisions
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
-        
         # how fast every single player move in every direction
         self.x_velocity = 0
         self.y_velocity = 0
 
         self.mask = None
-        
         self.direction = "left"
         self.animation_count = 0
+        
+        self.fall_count = 0
     
     def move(self, dx, dy):
         self.rect.x += dx
@@ -45,23 +71,26 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
             
             
-    def mvoe_right(self, velocity):
+    def move_right(self, velocity):
         self.x_velocity = velocity
         if self.direction != "right":
             self.direction = "right"
             self.animation_count = 0
             
     def loop(self, fps):
+        # self.y_velocity += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_velocity, self.y_velocity)
+        self.fall_count  += 1
         
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        # pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
         
 # get background information
 def get_background(name):
     image = pygame.image.load(join("assets", "background", name))
     _, _, width, height = image.get_rect()
-    
     tiles = []
     
     for i in range(WIDTH // width +1):
@@ -79,6 +108,14 @@ def draw(window, background, bg_image, player):
     player.draw(window)
     pygame.display.update()
 
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
@@ -89,12 +126,12 @@ def main(window):
     while run:
         clock.tick(FPS)  # run in 60 fps per second
         
-        for event in pygame.event.get():
-            
+        for event in pygame.event.get():            
             if event.type == pygame.QUIT:
                 run = False
                 break
-            
+        player.loop(FPS)
+        handle_move(player)
         draw(window, background, bg_image, player)
     pygame.quit()
     quit()
